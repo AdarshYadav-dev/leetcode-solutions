e36af91dd56f2265c28478f2ce8a666d73896b02
AdarshYadav-dev <adarsh98215@gmail.com>
2025-12-05 23:33:31 +0530

    solve Q7

--- Day4/Q7.js
+++ Day4/Q7.js
@@ -7,3 +7,30 @@
 
 // Input: a = "11", b = "1"
 // Output: "100"
+
+
+var addBinary = function(a, b) {
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = "";
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let sum = carry;
+
+        if (i >= 0) sum += a[i] - '0';
+        if (j >= 0) sum += b[j] - '0';
+
+        result = (sum % 2) + result;  // add current bit
+        carry = Math.floor(sum / 2); // update carry
+
+        i--;
+        j--;
+    }
+
+    return result;
+};
+
+
+console.log(addBinary("11", "1"));
+
