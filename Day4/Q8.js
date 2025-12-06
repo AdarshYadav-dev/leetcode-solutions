847930843b3ef793086ba009641dc09f16412a87
AdarshYadav-dev <adarsh98215@gmail.com>
2025-12-06 20:30:40 +0530

    solve Q8

---
+++ Day4/Q8.js
@@ -0,0 +1,25 @@
+// 349. Intersection of Two Arrays
+
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+
+
+var intersection = function(nums1,nums2){
+    let set1 = new Set(nums1);
+    let result = new Set();
+
+    for(let num of nums2){
+        if(set1.has(num)){
+            result.add(num);
+        }
+    }
+    return Array.from(result);
+}
+
+console.log(intersection([1,2,1,2],[1,2]));
