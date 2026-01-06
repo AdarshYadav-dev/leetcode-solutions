12c7f15588374a1c35715acdea5bcfbedee0758e
AdarshYadav-dev <adarsh98215@gmail.com>
2026-01-06 22:51:29 +0530

    solve Q27

---
+++ Day14/Q27.js
@@ -0,0 +1,23 @@
+// 744. Find Smallest Letter Greater Than Target
+
+let nextGreatestLetter = function(letters, target) {
+    let left = 0;
+    let right = letters.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (letters[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return letters[left % letters.length];
+};
+
+let letters = ["c", "f", "j"];
+let target = "a";
+
+console.log(nextGreatestLetter(letters, target));
